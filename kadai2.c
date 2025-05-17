cbb0d1a25742a5a580a4d619513d0523668c2c3b
saikawawawa <1116yoshiki1116@keio.jp>
2025-05-17 15:03:55 +0000

    jfd

--- kadai2.c
+++ kadai2.c
@@ -58,41 +58,37 @@ int read_promoter(char *filename){
   return gene_num;
 }
 
-int frequency(){
-  
-}
-int main(int argc, char* argv[]){
-  int seq_num = read_multi_seq(argv[1]); //１番目の引数で指定した転写因子の複数の結合部位配列を読み込む
-  int gene_num = read_promoter(argv[2]);  //２番目の引数で指定した遺伝子のプロモータ領域を読み込む
-
-  int num = strlen(g_motif[0]);
-  float hindo[ACGT][num];
+int frequency_matrix(int transcription_len, float frequency[ACGT][transcription_len]){
   int i=0, j=0;
   for(i=0; i<ACGT; i++){
-    for(j=0; j<num; j++){
-        hindo[i][j]=0;
-    }
-  }
-  for(j=0; j<num; j++){
-    for(i=0; i<MAX_SEQ_NUM; i++){
-        switch(g_motif[i][j]){
-            case 'A' : hindo[0][j]++; break;
-            case 'C' : hindo[1][j]++; break;
-            case 'G' : hindo[2][j]++; break;
-            case 'T' : hindo[3][j]++; break;
-        }
+    for(j=0; j<transcription_len; j++){
+        frequency[i][j]=0;
     }
   }
 
-  float m = hindo[0][0]+hindo[1][0]+hindo[2][0]+hindo[3][0]+4;
+  for(j=0; j<transcription_len; j++){
+    for(i=0; i<MAX_SEQ_NUM; i++){
+      switch(g_motif[i][j]){
+        case 'A' : frequency[0][j]++; break;
+        case 'C' : frequency[1][j]++; break;
+        case 'G' : frequency[2][j]++; break;
+        case 'T' : frequency[3][j]++; break;
+      }
+    }
+  }
+}
+
+int score_matrix(int transcription_len, float score[ACGT][transcription_len]){
+  int i=0, j=0;
+  float m = score[0][0]+score[1][0]+score[2][0]+score[3][0]+4;
   for(i=0; i<ACGT; i++){
-    for(j=0; j<num; j++){
-        hindo[i][j]=hindo[i][j]+1;
+    for(j=0; j<transcription_len; j++){
+        score[i][j]=score[i][j]+1;
     }
   }
   for(i=0; i<ACGT; i++){
-    for(j=0; j<num; j++){
-        hindo[i][j]=hindo[i][j]/m;
+    for(j=0; j<transcription_len; j++){
+        score[i][j]=score[i][j]/m;
     }
   }
 
@@ -100,50 +96,53 @@ int main(int argc, char* argv[]){
   float qCG = 4637676.0/(7519429+4637676+4637676+7519429);
   float q[ACGT]={qAT, qCG, qCG, qAT};
 
-  float hindo_1[4][num];
   for(i=0; i<ACGT; i++){
-    for(j=0; j<num; j++){
-        hindo_1[i][j]=0;
+    for(j=0; j<transcription_len; j++){
+        score[i][j]=log(score[i][j]/q[i]);
     }
   }
-  for(i=0; i<ACGT; i++){
-    for(j=0; j<num; j++){
-        hindo_1[i][j]=log(hindo[i][j]/q[i]);
-    }
-  }
+}
 
-  int len = strlen(g_pro[0].seq);
+int binding_site(int transcription_len,  float binding[ACGT][transcription_len]){
+  int i=0, j=0, k=0;
+  int promoter_len=strlen(g_pro[0].seq);
   int pos=0;
   float score=0.0;
-  char hairetu[num+1];
-  char max_score_hairetu[num+1];
+  char array[transcription_len+1];
   for(i=0; i<MAX_GENE_NUM; i++){
-    printf("pro:%s\n",g_pro[i].name);
-    for(j=0; j<len-num; j++){
+    for(j=0; j<promoter_len-transcription_len; j++){
       score=0.0;
-      for(int k=0; k<num; k++){
-        hairetu[k]=g_pro[i].seq[j+k];
-          if(g_pro[i].seq[j+k]=='A'){
-            score+=hindo_1[0][k];
-          }
-          if(g_pro[i].seq[j+k]=='C'){
-            score+=hindo_1[1][k];
-          }
-          if(g_pro[i].seq[j+k]=='G'){
-            score+=hindo_1[2][k];
-          }
-          if(g_pro[i].seq[j+k]=='T'){
-            score+=hindo_1[3][k];
-          }
-        }
-        hairetu[num] = '\0';
-
-        if (score >= threshold) {
-          printf("pos:%d\n",j+1);
-          printf("hit(%s)=%f\n\n",hairetu,score);
+      for(k=0; k<transcription_len; k++){
+        array[k]=g_pro[i].seq[j+k];
+        switch(g_pro[i].seq[j+k]){
+          case 'A' : score+=binding[0][k]; break;
+          case 'C' : score+=binding[1][k]; break;
+          case 'G' : score+=binding[2][k]; break;
+          case 'T' : score+=binding[3][k]; break;
         }
       }
+      array[transcription_len] = '\0';
+      
+      if (score >= threshold) {
+        printf("pro: %s\n",g_pro[i].name);
+        printf("pos: %d\n",j+1);
+        printf("hit(%s)= %f\n\n",array,score);
+      }
     }
+  }
+  
+}
+
+int main(int argc, char* argv[]){
+  int seq_num = read_multi_seq(argv[1]); //１番目の引数で指定した転写因子の複数の結合部位配列を読み込む
+  int gene_num = read_promoter(argv[2]);  //２番目の引数で指定した遺伝子のプロモータ領域を読み込む
+
+  int transcription_factor_len=strlen(g_motif[0]);
+  float matrix[ACGT][transcription_factor_len];
+
+  frequency_matrix(transcription_factor_len, matrix);
+  score_matrix(transcription_factor_len, matrix);
+  binding_site(transcription_factor_len, matrix);
   
   return 0;
 }
